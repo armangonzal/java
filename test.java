c5ea46fe5e12bfa7f16843785f2ee752c45ca744
Armando Gonzalez <armangonzal@gmail.com>
2017-02-10 22:04:45 -0600

    New line

--- test.java
+++ test.java
@@ -1,7 +1,13 @@
+import java.util.Scanner;
+
 public class MyTest {
        public static void main(String[] args){
        
-              System.out.println("Armando");
+              System.out.println("Enter a Number:");
+              Scanner Kbd = new Scanner(System.in);
+              int number = kbd.getInt();
+              System.out.println(number);
+
 
        }
 }
