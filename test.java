18cab90f0cd945ec39e2219c761eee5e9b69a5ec
Armando Gonzalez <armangonzal@gmail.com>
2017-02-15 22:21:58 -0600

    edit

--- test.java
+++ test.java
@@ -2,7 +2,6 @@ import java.util.Scanner;
 
 public class Test {
        public static void main(String[] args){
-              System.out.println("Armando");
               System.out.println("Enter a Number:");
               Scanner kbd = new Scanner(System.in);
               int number = kbd.nextInt();
