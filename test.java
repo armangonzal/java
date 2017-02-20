927722ca99f29e2fd65d3e1473f244cab8244d7d
Armando Gonzalez <armangonzal@gmail.com>
2017-02-20 11:51:21 -0600

    ImageTest java added

--- test.java
+++ test.java
@@ -1,15 +1,19 @@
 import java.util.Scanner;
 
+
+
 public class Test {
        public static void main(String[] args){
+
+              
               System.out.println("Enter a Number:");
               Scanner kbd = new Scanner(System.in);
-              int number = kbd.nextInt();
+              int number = kbd.nextInt() * 2;
               System.out.println(number);
               
-              System.out.println("Enter another Number:");
-              Scanner kbd02 = new Scanner(System.in);
-              int number02 = kbd02.nextInt();
-              System.out.println(number02);
-       }
+
+
+
+              }
+
 }
