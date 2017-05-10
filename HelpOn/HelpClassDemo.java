89341dbadcea8e3789ed9444a81220de43077369
Armando Gonzalez <armangonzal@gmail.com>
2017-05-09 21:57:55 -0500

    HelpClassDemo completed

--- HelpOn/HelpClassDemo.java
+++ HelpOn/HelpClassDemo.java
@@ -42,5 +42,47 @@ class Help {
             System.out.println();
     }
 
+    void showMenu() {
+        System.out.println("Help on:");
+        System.out.println("    1. if");
+        System.out.println("    2. switch");
+        System.out.println("    3. for");
+        System.out.println("    4. while");
+        System.out.println("    5. do-while");
+        System.out.println("    6. break");
+        System.out.println("    7. continue\n");
+        System.out.println("Choose one (q to quit): ");
+    }
+
+    boolean isValid(int ch){
+        if(ch < '1' | ch > '7' & ch != 'q') return false;
+        else return true;
+    }
      
+}
+
+class HelpClassDemo {
+    public static void main(String args[])
+    throws java.io.IOException {
+        char choice, ignore;
+        Help hipobj = new Help();
+
+        for(;;) {
+            do {
+                hipobj.showMenu();
+
+                choice = (char) System.in.read();
+
+                do {
+                    ignore = (char) System.in.read();
+                } while(ignore != '\n');
+            } while( !hipobj.isValid(choice) );
+
+            if(choice == 'q') break;
+
+            System.out.println("\n");
+
+            hipobj.helpOn(choice);
+        }
+    }
 }
