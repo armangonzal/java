05686279b918153eaa38cbd8a579f7a568dbe4ef
Armando Gonzalez <armangonzal@gmail.com>
2017-06-20 20:59:07 -0500

    tutorial exercise

---
+++ udemy/tutorial_01/Main.java
@@ -0,0 +1,16 @@
+public class Main {
+
+  public static void main(String[] args) {
+    youstupid(false);
+  }
+  
+
+  public static void youstupid(boolean stupid){
+    if(stupid) {
+      System.out.println("you stupid!");
+    } else {
+      System.out.println("you still stupid!");
+    }
+  }
+
+}
