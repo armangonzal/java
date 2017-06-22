6695687d588ea2a0d4ac9f1d2b7c2a02dcfedea7
Armando Gonzalez <armangonzal@gmail.com>
2017-06-21 23:01:36 -0500

    java overloading exercise

--- udemy/tutorial_01/Main.java
+++ udemy/tutorial_01/Main.java
@@ -1,15 +1,60 @@
 public class Main {
 
   public static void main(String[] args) {
-    youstupid(false);
+    mixpaint(true, false, true);
+    mixpaint(true, true);
+    mixpaint(true);
   }
   
 
-  public static void youstupid(boolean stupid){
-    if(stupid) {
-      System.out.println("you stupid!");
-    } else {
-      System.out.println("you still stupid!");
+  public static void mixpaint(boolean red, boolean blue, boolean yellow){
+    if(red && blue && yellow) {
+      System.out.println("Black");
+    } 
+    else if (red && blue && yellow == false){
+      System.out.println("Violet");
+    }
+    else if (red && blue == false && yellow){
+      System.out.println("Orange");
+    }
+    else if (red == false && blue && yellow){
+      System.out.println("Green");
+    }
+    else if (red && blue == false && yellow == false){
+      System.out.println("Red");
+    }
+    else if (red == false && blue == false && yellow){
+      System.out.println("Yellow");
+    }
+    else if (red == false && blue && yellow == false){
+      System.out.println("Blue");
+    }
+    else {
+      System.out.println("No paint!");
+    }
+  }
+
+  public static void mixpaint(boolean red, boolean blue){
+    if(red && blue) {
+      System.out.println("Violet");
+    }
+    else if (red && blue == false){
+      System.out.println("Red");
+    }
+    else if (red == false && blue){
+      System.out.println("Blue");
+    }
+    else {
+      System.out.println("No paint!");
+    }
+  }
+
+  public static void mixpaint(boolean red){
+    if (red){
+      System.out.println("Red");
+    }
+    else {
+      System.out.println("No paint!");
     }
   }
 
