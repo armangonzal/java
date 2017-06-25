d14003beaf33115c9021a793c9e8f2c1d0548de6
Armando Gonzalez <armangonzal@gmail.com>
2017-06-24 19:03:09 -0500

    added logic to color mix code

--- udemy/tutorial_01/Main.java
+++ udemy/tutorial_01/Main.java
@@ -9,40 +9,65 @@ public class Main {
   public static void askforpaint(){
     boolean redpaint = askforcolor("red");
     
-    System.out.println("More Paint? y/n:");
+    System.out.println("More Paint? Enter:\n0: no\n1: yes");
     Scanner sc1 = new Scanner(System.in);
-    String morepaint1 = sc1.nextLine();
+    int morepaint1;
 
-    if (checkanswer(morepaint1)){
-      boolean bluepaint = askforcolor("blue");
-    } else {
-      System.out.println("Okay no more paint");
+    if(sc1.hasNextInt()) { 
+      morepaint1 = sc1.nextInt();
+      if (checkanswer(morepaint1)){
+         boolean bluepaint = askforcolor("blue");
+        System.out.println("More Paint? Enter:\n0: no\n1: yes");
+        Scanner sc2 = new Scanner(System.in);
+        int morepaint2;
+
+        if(sc2.hasNextInt()) { 
+          morepaint2 = sc2.nextInt();
+          if (checkanswer(morepaint1)){
+            boolean yellowpaint = askforcolor("yellow");
+            mixpaint(redpaint, bluepaint, yellowpaint);
+          } else {
+            System.out.println("Okay no more paint");
+            mixpaint(redpaint, bluepaint);
+          }
+        }
+      } else {
+        System.out.println("Okay no more paint");
+        mixpaint(redpaint);
+      }
     }
+
+
 
 
   }
 
 public static boolean askforcolor(String color){
-  System.out.println("Do you want " + color + " paint? y/n:");
+  System.out.println("Do you want " + color + " paint? Enter:\n0: no\n1: yes");
     Scanner sc = new Scanner(System.in);
-    String coloranswer = sc.nextLine();
-    if (acceptanswer(coloranswer)){
-      return checkanswer(coloranswer);
+    int coloranswer;
+    if(sc.hasNextInt()) {
+      coloranswer = sc.nextInt();
+      if (acceptanswer(coloranswer)){
+        return checkanswer(coloranswer);
+      } else {
+          return false;
+      }
     } else {
       return false;
     }
 }  
 
-public static boolean acceptanswer(String answer){
-  if (answer == "y" || answer == "n"){
+public static boolean acceptanswer(int answer){
+  if (answer == 1 || answer == 0){
     return true;
   } else {
     return false;
   }
 }
 
-  public static boolean checkanswer(String answer){
-    if(answer == "y"){
+  public static boolean checkanswer(int answer){
+    if(answer == 1){
       return true;
     } else {
       System.out.println("Your answer is false: " + answer);
@@ -52,25 +77,25 @@ public static boolean acceptanswer(String answer){
 
   public static void mixpaint(boolean red, boolean blue, boolean yellow){
     if(red && blue && yellow) {
-      System.out.println("Black");
+      System.out.println("Your final color is Black");
     } 
     else if (red && blue && yellow == false){
-      System.out.println("Violet");
+      System.out.println("Your final color is Violet");
     }
     else if (red && blue == false && yellow){
-      System.out.println("Orange");
+      System.out.println("Your final color is Orange");
     }
     else if (red == false && blue && yellow){
-      System.out.println("Green");
+      System.out.println("Your final color is Green");
     }
     else if (red && blue == false && yellow == false){
-      System.out.println("Red");
+      System.out.println("Your final color is Red");
     }
     else if (red == false && blue == false && yellow){
-      System.out.println("Yellow");
+      System.out.println("Your final color is Yellow");
     }
     else if (red == false && blue && yellow == false){
-      System.out.println("Blue");
+      System.out.println("Your final color is Blue");
     }
     else {
       System.out.println("No paint!");
@@ -79,13 +104,13 @@ public static boolean acceptanswer(String answer){
 
   public static void mixpaint(boolean red, boolean blue){
     if(red && blue) {
-      System.out.println("Violet");
+      System.out.println("Your final color is Violet");
     }
     else if (red && blue == false){
-      System.out.println("Red");
+      System.out.println("Your final color is Red");
     }
     else if (red == false && blue){
-      System.out.println("Blue");
+      System.out.println("Your final color is Blue");
     }
     else {
       System.out.println("No paint!");
@@ -94,7 +119,7 @@ public static boolean acceptanswer(String answer){
 
   public static void mixpaint(boolean red){
     if (red){
-      System.out.println("Red");
+      System.out.println("Your final color is Red");
     }
     else {
       System.out.println("No paint!");
