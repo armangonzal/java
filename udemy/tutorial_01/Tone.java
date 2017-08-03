7109ad8944a05999a5533059add326c598eb5240
Armando Gonzalez <armangonzal@gmail.com>
2017-08-02 21:15:40 -0500

    modefied Tone class

--- udemy/tutorial_01/Tone.java
+++ udemy/tutorial_01/Tone.java
@@ -4,4 +4,15 @@ public class Tone {
   public static void askforTone(){
     String[] grayValues = {"white", "light", "mid", "dark", "black"};
   }
+
+  public static void askforLevel(){
+    System.out.println("How light or dark?");
+    Scanner sc = new Scanner(System.in);
+    int toneAnwser = 0;
+
+    if(sc.hasNextInt()) {
+
+    }
+  }
+  
 }
