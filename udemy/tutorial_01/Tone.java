46f84b21bea6c78ee07c8f6cd38b8f43d3b3e1a9
Armando Gonzalez <armangonzal@gmail.com>
2017-08-22 22:15:00 -0500

    ask for tone val 0 to 10

--- udemy/tutorial_01/Tone.java
+++ udemy/tutorial_01/Tone.java
@@ -4,18 +4,26 @@ public class Tone {
 
   public static void askforTone(){
     int toneVal;
-    String[] valList = {"white", "light", "gray", "dark", "black"};
+    int satVal;
 
-    System.out.println("What tone do you want?");
-    Scanner sc1 = new Scanner(System.in);
-    if(sc1.hasNextInt()){
-      toneVal = sc1.nextInt();
-      if(toneVal <= 4){
+    askforToneVal();
+
+
+  }
+
+  public static void askforToneVal(){
+    int toneVal;
+    System.out.println("What tone do you want? \nEnter a number from 0 to 10:");
+    Scanner scTone = new Scanner(System.in);
+    if(scTone.hasNextInt()){
+      toneVal = scTone.nextInt();
+      if(toneVal >= 0 && toneVal <= 10){
          System.out.println("Acceptable tone value.");
       } else {
         System.out.println("Not an acceptable tone value.");
       }
     }
+
   }
 
 }
