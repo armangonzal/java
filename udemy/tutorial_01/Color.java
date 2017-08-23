a9b100292d3336e498991a9ce872a3714a5c7e8c
Armando Gonzalez <armangonzal@gmail.com>
2017-08-23 09:48:08 -0500

    color prompt added

--- udemy/tutorial_01/Color.java
+++ udemy/tutorial_01/Color.java
@@ -1,10 +1,27 @@
 public class Color {
 
-  int blue;
-  int yellow;
-  int red;
+  int hue = 0;
   int saturation;
-  int tone;
+  int gray;
 
-  
+  public static String idColor(int hue){
+    int i = hue/10;
+    switch(i) {
+      case 0:
+        return "Red";
+      case 1:
+        return "Orange";
+      case 2:
+        return "Yellow";
+      case 3:
+        return "Green";
+      case 4:
+        return "Blue";
+      case 5:
+        return "Violet";
+      default:
+        return "Not visible to human eye!";
+    }
+
+  }
 }
--- udemy/tutorial_01/Main.java
+++ udemy/tutorial_01/Main.java
@@ -2,11 +2,20 @@ import java.util.Scanner;
 public class Main {
 
   public static void main(String[] args) {
-      Tone tone01 = new Tone();
-      Paint paint01 = new Paint();
+    Tone tone01 = new Tone();
+    Paint paint01 = new Paint();
+    Color testcolor = new Color();
 
 
-    tone01.askforTone();
-    paint01.askforpaint();
+    System.out.println("Enter a hue from 0 to 59:");
+    Scanner SChue = new Scanner(System.in);
+    if(SChue.hasNextInt()){
+      testcolor.hue = SChue.nextInt();
+      System.out.println(testcolor.idColor(testcolor.hue));
+    } else {
+      System.out.println("Not an acceptable hue value");
+    }
+
+    //paint01.askforpaint();
   }
 }
