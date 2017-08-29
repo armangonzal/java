61172afa8c4c33d1d96ff23ff29de903b343084a
Armando Gonzalez <armangonzal@gmail.com>
2017-08-28 21:05:55 -0500

    askforcolor method in Color

--- udemy/tutorial_01/Color.java
+++ udemy/tutorial_01/Color.java
@@ -1,34 +1,19 @@
 public class Color {
 
-  int hue = 0;
-  int saturation;
-  int gray;
-  int tone;
+  int red = 0;
+  int green = 0;
+  int blue = 0;
 
-  public static String idHue(int hue){
-    int i = hue/10;
-    switch(i) {
-      case 0:
-        return "Red";
-      case 1:
-        return "Orange";
-      case 2:
-        return "Yellow";
-      case 3:
-        return "Green";
-      case 4:
-        return "Blue";
-      case 5:
-        return "Violet";
-      default:
-        return "Not visible to human eye!";
-    }
-
+  public static int askforcolor(int red, int green, int blue){
+    
   }
 
-  public static int idTone(int saturation, int gray, int tone){
-      tone = saturation * gray;
-      return tone;
-  }
+  public static String idHue(int red, int green, int blue){
+
+    String hexR = Integer.toHexString(red);
+    String hexG = Integer.toHexString(green);
+    String hexB = Integer.toHexString(blue);
+
+    System.out.println("Your hex color is: #" + hexR + hexG + hexB);
 
 }
--- udemy/tutorial_01/Main.java
+++ udemy/tutorial_01/Main.java
@@ -2,28 +2,10 @@ import java.util.Scanner;
 public class Main {
 
   public static void main(String[] args) {
-    Tone tone01 = new Tone();
+
     Paint paint01 = new Paint();
     Color testcolor = new Color();
 
-
-    System.out.println("Enter a hue from 0 to 59:");
-    Scanner SChue = new Scanner(System.in);
-    if(SChue.hasNextInt()){
-      testcolor.hue = SChue.nextInt();
-      System.out.println(testcolor.idHue(testcolor.hue));
-    } else {
-      System.out.println("Not an acceptable hue value");
-    }
-
-    System.out.println("Enter a saturation value from 0 to 100");
-    Scanner SCsat = new Scanner(System.in);
-    if(SCsat.hasNextInt()){
-      testcolor.saturation = SCsat.nextInt();
-      System.out.println("Saturation value is: " + testcolor.saturation);
-    } else {
-      System.out.println("That is not an acceptable saturation value.");
-    }
 
 
     //paint01.askforpaint();
