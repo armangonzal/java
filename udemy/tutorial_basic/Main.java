638d6a4fe4c94570ee5a28bbee7de2a59d1325a0
Armando Gonzalez <armangonzal@gmail.com>
2017-09-05 19:13:22 -0500

    tutorial excerices

--- udemy/tutorial_basic/Main.java
+++ udemy/tutorial_basic/Main.java
@@ -1,8 +1,19 @@
 public class Main {
   public static void main(String[] args) {
 
-    int myNum = 29;
-    byte mybyte = 123;
-    short myShort = 1000;
+    char myChar = '\u00A9';
+
+    String myString = "\u00A9 symbol";
+
+    System.out.println(myString);
+
+    boolean isP = true;
+
+    boolean wasP = isP ? true : false;
+
+    if(wasP){
+      System.out.println("Was P!!!");
+    }
+
   }
 }
