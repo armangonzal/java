dbb9f11fa115769ad8963884c6f79c0a39e9ceb8
Armando Gonzalez <armangonzal@gmail.com>
2017-09-13 10:23:21 -0500

    moved line of code around

--- udemy/tutorial_01/Main.java
+++ udemy/tutorial_01/Main.java
@@ -5,16 +5,14 @@ import java.awt.Rectangle;
 public class Main {
 
   public static void main(String[] args) {
-    //Paint paint01 = new Paint();
-    
-    //paint01.askforpaint();
+
+    DrawColor dc = new DrawColor();
+
     JFrame window = new JFrame();
     window.setSize(640, 480);
     window.setTitle("Color Window");
     window.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
     window.setVisible(true);
-
-    DrawColor dc = new DrawColor();
     window.add(dc);
   }
 
