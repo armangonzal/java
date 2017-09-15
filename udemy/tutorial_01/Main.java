917309b95765f4c19fe1c8bc5e662b6f21fd6904
Armando Gonzalez <armangonzal@gmail.com>
2017-09-14 21:33:43 -0500

    created prompt class

--- udemy/tutorial_01/Main.java
+++ udemy/tutorial_01/Main.java
@@ -6,14 +6,8 @@ public class Main {
 
   public static void main(String[] args) {
 
-    DrawColor dc = new DrawColor();
-
-    JFrame window = new JFrame();
-    window.setSize(640, 480);
-    window.setTitle("Color Window");
-    window.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-    window.setVisible(true);
-    window.add(dc);
+    Prompt mp = new Prompt();
+    mp.askforwindow();
   }
 
 }
---
+++ udemy/tutorial_01/Prompt.java
@@ -0,0 +1,33 @@
+import java.util.Scanner;
+import javax.swing.JFrame;
+import java.awt.Rectangle;
+
+public class Prompt {
+
+
+  public void askforwindow(){
+    System.out.println("Do you want a color? \n1: yes \n2: no");
+
+    Scanner a1 = new Scanner(System.in);
+    int answer = a1.nextInt();
+
+    if(answer == 1){
+      DrawColor dc = new DrawColor();
+      JFrame window = new JFrame();
+      window.setSize(640, 480);
+      window.setTitle("Color Window");
+      window.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+      window.setVisible(true);
+      window.add(dc);
+    } else if (answer == 2){
+      System.out.println("Okay no color.");
+    } else {
+      System.out.println("Not a valid answer. ");
+      askforwindow();
+    }
+
+
+  }
+
+
+}
