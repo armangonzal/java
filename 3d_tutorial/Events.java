785690632be94f0d4169178977c0bec4e41398b7
Armando Gonzalez <armangonzal@gmail.com>
2017-09-21 06:52:57 -0500

    button tutorial with second event

--- 3d_tutorial/Events.java
+++ 3d_tutorial/Events.java
@@ -7,28 +7,61 @@ import javax.swing.JLabel;
 import javax.swing.JButton;*/
 
 public class Events extends JFrame {
-    private JLabel label;
-    private JButton button;
+    private JLabel label1;
+    private JLabel label2;
+    private JButton button1;
+    private JButton button2;
+    private int x =0, y = 0;
 
     public Events(){
         setLayout(new FlowLayout());
 
-        button = new JButton("Click for text");
-        add(button);
+        button1 = new JButton("Click for text");
+        add(button1);
 
-        label = new JLabel("");
-        add(label);
+        label1 = new JLabel("");
+        add(label1);
+
+        button2 = new JButton("Click for more text");
+        add(button2);
+
+        label2 = new JLabel("");
+        add(label2);
 
         Event e = new Event();
-        button.addActionListener(e);
+        button1.addActionListener(e);
+
+        Event2 ev = new Event2();
+        button2.addActionListener(ev);
     }
 
 
     public class Event implements ActionListener {
         public void actionPerformed(ActionEvent e) {
-            label.setText("You are my favorite");
+          if(x == 0){
+            label1.setText("You are my favorite");
+            x = 1;
+          } else {
+            label1.setText("");
+            x = 0;
+          }
         }
     }
+
+
+    public class Event2 implements ActionListener {
+        public void actionPerformed(ActionEvent ev){
+          if(y == 0){
+            label2.setText("So are you!");
+            y = 1;
+          } else {
+            label2.setText("");
+            y = 0;
+          }
+
+        }
+    }
+
 
     public static void main (String args[]){
         Events gui = new Events();
