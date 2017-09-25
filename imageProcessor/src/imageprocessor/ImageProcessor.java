08b4231d75bb3826059b72fa20cdbccc10583302
Armando Gonzalez <armangonzal@gmail.com>
2017-09-24 19:02:08 -0500

    netBeans test

--- imageProcessor/src/imageprocessor/ImageProcessor.java
+++ imageProcessor/src/imageprocessor/ImageProcessor.java
@@ -5,17 +5,49 @@
  */
 package imageprocessor;
 
+import java.awt.*;
+import java.awt.event.*;
+import javax.swing.*;
+
 /**
  *
  * @author MandoG
  */
-public class ImageProcessor {
+public class ImageProcessor extends JFrame{
+    JLabel label;
+    JButton button;
+    
+    public ImageProcessor(){
+        setLayout(new FlowLayout());
+        
+        button = new JButton("Click");
+        add(button);
+        
+        label = new JLabel("");
+        add(label);
+        
+        ButtonEvent e = new ButtonEvent();
+        button.addActionListener(e);
+    }
 
     /**
      * @param args the command line arguments
      */
-    public static void main(String[] args) {
-        // TODO code application logic here
-    }
     
+    public class ButtonEvent implements ActionListener{
+        public void actionPerformed(ActionEvent e){
+            label.setText("Hello");
+        }
+}
+    
+    public static void main(String args[]) {
+        // TODO code application logic here
+        ImageProcessor gui = new ImageProcessor();
+        gui.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        gui.setTitle("Chub Chubs");
+        gui.setSize(300, 600);
+        gui.setVisible(true);
+        
+    }  
+   
 }
