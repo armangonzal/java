d2b26264fff37f7157e7d4fc90c4654552a023a9
Armando Gonzalez <armangonzal@gmail.com>
2018-06-26 15:25:54 -0500

    pg 685

--- practice/JavaRef.java
+++ practice/JavaRef.java
@@ -4,24 +4,28 @@ class JavaRef {
 
    public static void main(String args[])
    {
-      String source = "Now is the time for all good men\n"
-               + " to come to the aid of their country\n"
-               + " and pay their due taxes.";
-      byte buf[] = source.getBytes();
+      String tmp = "abcdefghijklmnopqrstuvwxyz";
+      byte b[] = tmp.getBytes();
       
-      try (FileOutputStream f0 = new FileOutputStream("file1.txt");
-           FileOutputStream f1 = new FileOutputStream("file2.txt");
-           FileOutputStream f2 = new FileOutputStream("file3.txt") )
+      ByteArrayInputStream input1 = new ByteArrayInputStream(b);
+      ByteArrayInputStream input2 = new ByteArrayInputStream(b, 0, 3);
+      
+      for( int i=0; i<26; i++)
       {
-         for(int i=0; i < buf.length; i+=4) f0.write(buf[i]);
-         
-         f1.write(buf);
-         
-         f2.write(buf, buf.length-buf.length/2, buf.length/2);
-      }
-      catch(IOException e)
-      {
-         System.out.println("An I/O Error Occurred");
+         int c;
+         while((c = input1.read()) != -1)
+         {
+            if (i <= 12)
+            {
+               System.out.print((char) c);
+            }
+            else
+            {
+               System.out.print(Character.toUpperCase((char) c));
+            }
+         }
+         System.out.println();
+         input1.reset();
       }
    }
 }
