472d37518d86a570605bcb9bd1c7ae1f039ca648
armangonzal <armangonzal@gmail.com>
2018-07-14 23:34:02 -0500

    strManip first

---
+++ practice/StrManip.java
@@ -0,0 +1,23 @@
+import java.io.*;
+import java.util.*;
+
+public class StrManip {
+
+    public static void main(String[] args) {
+        
+        Scanner sc=new Scanner(System.in);
+        String A=sc.next();
+        String B=sc.next();
+        /* Enter your code here. Print output to STDOUT. */
+        
+        int sumLen = A.length()+B.length();
+        String grA = "Yes";
+        if(A.compareTo(B) < 0) grA = "No";
+        
+        String capA = A.substring(0,1).toUpperCase() + A.substring(1,A.length());
+        String capB = B.substring(0,1).toUpperCase() + B.substring(1,B.length());
+        
+        System.out.printf("%d%n%s%n%s %s%n",sumLen, grA, capA, capB);
+        
+    }
+}
---
+++ practice/Test.java
@@ -0,0 +1,21 @@
+import java.io.*;
+
+class Test
+{
+   public static void main(String[] args)
+   {
+      Gen<Integer> iOb;
+      iOb = new Gen<Integer>(88);
+      iOb.showType();
+      
+      int v = iOb.getob();
+      System.out.println("value: "+v);
+      System.out.println();
+      
+      Gen<String> strOb = new Gen<String>("Generics Test");
+      strOb.showType();
+      String str = strOb.getob();
+      System.out.println("value: "+ str);
+   }
+
+}
