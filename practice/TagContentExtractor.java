dba6f49d8559d7242f0717914ffd0972ac9bf95c
armangonzal <armangonzal@gmail.com>
2018-07-16 16:20:41 -0500

    using matcher
    groups indicate parantheses

---
+++ practice/TagContentExtractor.java
@@ -0,0 +1,34 @@
+import java.io.*;
+import java.util.*;
+import java.text.*;
+import java.math.*;
+import java.util.regex.*;
+
+public class TagContentExtractor{
+	public static void main(String[] args){
+		
+		Scanner in = new Scanner(System.in);
+		int testCases = Integer.parseInt(in.nextLine());
+		while(testCases>0){
+			String line = in.nextLine();
+			
+          	//Write your code here
+            
+            String regex = "[<]([a-zA-Z0-1]+)[>](.+)[<][/]\\1[>]";
+            
+            Pattern p = Pattern.compile(regex);
+            
+            Matcher m = p.matcher(line);
+            
+            while(m.find())
+            {
+               String sub = line.replaceAll(m.group(), m.group(2));
+               System.out.println(sub);
+            }
+            //if(line.matches(regex)) System.out.println(line);
+            //else System.out.println("Invalid");
+			
+			testCases--;
+		}
+	}
+}
